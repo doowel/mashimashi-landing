9e9db549c027505f839a0a782fe463f01016ae44
Paipo Tang <paipo.tang@wolt.com>
2023-11-07 22:03:27 +0900

    Update email modal interaction

--- src/pages/Monten.jsx
+++ src/pages/Monten.jsx
@@ -23,7 +23,10 @@ function Monten() {
           }),
         }
       );
+      alert('Thank you for subscribing! We will keep you updated.')
+      setSubscriberEmail("");
     } catch (error) {
+      alert('Something went wrong, please try again later.')
       console.log(error);
     }
   };
@@ -56,21 +59,21 @@ function Monten() {
         {/*  Site header */}
         <MontenHeader />
         {/*  Page content */}
-        <main className="px-6 md:px-36 pt-32 md:pt-52 bg-white flex flex-col xl:flex-row font-inter">
+        <main className="px-6 md:px-36 pt-32 md:pt-52 bg-white flex flex-col xl:flex-row xl:justify-between font-inter">
           <div className="max-w-3xl xl:mr-24">
             <h1 className="text-4xl md:text-6xl font-bold mb-14">悩んでいる時、 みんなの意見が気にする？</h1>
             <h2 className="text-xl mb-20">Monten は、好奇心の世界への扉を開き、あなたの興味をそそるものなら何でも、匿名で世間の声を探ります。決断する自信を求めている人も、単純に楽しい時間を求めている人も、Monten は無限の可能性への入り口です。</h2>
-            <form className="flex gap-3" onSubmit={handleSubmit}>
+            <form className="flex gap-3 flex-col lg:flex-row" onSubmit={handleSubmit}>
               <input
                 type="text"
-                className="w-full max-w-xs border border-gray-300 px-4 py-2 rounded-md"
+                className="w-full border border-gray-300 pl-4 py-2 rounded-md"
                 placeholder="Your email"
                 onChange={(event) => setSubscriberEmail(event.target.value)}
                 value={subscriberEmail}
               />
               <button
                 type="submit"
-                className="text-white w-36 h-11 rounded-3xl bg-gradient-to-r from-blue-400 via-purple-600 to-pink-500"
+                className="text-white w-full lg:w-36 h-11 rounded-3xl bg-gradient-to-r from-blue-400 via-purple-600 to-pink-500"
               >
                 Check it out
               </button>
